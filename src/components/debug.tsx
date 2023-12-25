764fd85883321e42fd3ab5bf1b81545cbb8d8ad0
Koen Rijpstra <k.rijpstra@gmail.com>
2023-12-25 21:21:24 +0100

    Disable @typescript-eslint/no-non-null-assertion

--- src/components/debug.tsx
+++ src/components/debug.tsx
@@ -29,7 +29,9 @@ function parseSignInMessageText(text: string) {
     if (!groups) return null;
 
     return {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         domain: groups.domain!,
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         address: groups.address!,
         statement: groups.statement,
         uri: groups.uri,
